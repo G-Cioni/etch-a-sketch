9074c513269cac09b15d9c0ee3fa7fdec96ef874
Gianluca Cioni <gianlucacioni93@gmail.com>
2021-07-13 03:02:02 +0200

    grid is now showing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 for (i = 1; i <= 16; i++){
     let div = document.createElement("div")
-    div.setAttribute("id", `div-${i}`)
+    div.setAttribute("id", `div${i}`)
+    div.textContent = "I'm a div"
     container.appendChild(div)
 }
