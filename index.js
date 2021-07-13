0766d4713084592195211806fb6d7744a7839534
Gianluca Cioni <gianlucacioni93@gmail.com>
2021-07-13 14:53:34 +0200

    grid is now working correctly and can be modified with js

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-for (i = 1; i <= 16; i++){
+let quantity = 8
+
+document.querySelector("#container").style.gridTemplateColumns = `repeat(${quantity}, 1fr)`
+document.querySelector("#container").style.gridTemplateRows = `repeat(${quantity}, 1fr)`
+
+for (i = 1; i <= Math.pow(quantity, 2); i++){
     let div = document.createElement("div")
     div.setAttribute("id", `div${i}`)
-    div.textContent = "I'm a div"
+
     container.appendChild(div)
 }
